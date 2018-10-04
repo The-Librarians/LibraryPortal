1972ff1037ade57daa95685a5bc285550f3ecc04
blnicholson <blnicholson13@gmail.com>
2018-10-04 18:52:59 -0500

    fixed seeds

--- models/usersSeeds.sql
+++ models/usersSeeds.sql
@@ -1,14 +1,15 @@
 USE libraryportal_db;
 
 INSERT INTO users (name, phone, email, cardNumber, password, isChild, isAdult, createdAt, updatedAt)
-VALUE ("Brandy Nicholson", "214-555-7845", "brandy.nicholson@thelibrarians.com", 15234, "Welcome123", false, true, 2018-10-01, 2018-10-01,
-	   "Alex Bowen", "214-555-1569", "alex.bowen@thelibrarians.com", 15235, "Welcome123", false, true, 2018-10-01, 2018-10-01,
-       "David Penberthy", "214-555-7125", "david.penberthy@thelibrarians.com", 15236, "Welcome123", false, true, 2018-10-01, 2018-10-01,
-       "Sadegh Sachedina", "214-555-3369", "sadegh.sachedina@thelibrarians.com", 15237,"Welcome123", false, true, 2018-10-01, 2018-10-01,
-       "Aaron, Davis", "214-555-5489", "aaron.davis@thelibrarians.com", 15238, "Welcome123", false, true, 2018-10-01, 2108-10-01,
-       "Kellen Weigel", "214-555-3326", "k.weigel@gmail.com", 15239, "Welcome123", true, false, 2018-10-01, 2018-10-01,
-       "Riley Weigel", "214-555-3327", "r.weigel@gmail.com", 15300, "Welcome123", true, false, 2018-10-01, 2018-10-01,
-       "Brooklyn Nicholson", "214-555-2563", "b.nicholson@gmail.com", 15301,"Welcome123", true, false, 2018-10-01, 2018-10-01,
-       "Stacy Anderews", "214-555-8153", "s.andrews@gmail.com", 15302, "Welcome123", true, false, 2018-10-01, 2018-10-01,
-       "Katelyn Nicholson", "214-555-8152", "kate.nicholson@gmail.com", 15303, "Welcome123", false, true, 2108-10-01, 2018-10-01
-       )
+VALUES ("Brandy Nicholson","972-555-1298","brandy.nicholson@thelibrarians.com", 15234, "Welcome123", false, true, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+	   ("Alex Bowen","214-555-1569", "alex.bowen@thelibrarians.com", 15235, "Welcome123", false, true, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+	   ("David Penberthy", "214-555-7125", "david.penberthy@thelibrarians.com", 15236, "Welcome123", false, true, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Sadegh Sachedina", "214-555-3369", "sadegh.sachedina@thelibrarians.com", 15237,"Welcome123", false, true, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Aaron, Davis", "214-555-5489", "aaron.davis@thelibrarians.com", 15238, "Welcome123", false, true, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Kellen Weigel", "214-555-3326", "k.weigel@gmail.com", 15239, "Welcome123", true, false, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Riley Weigel", "214-555-3327", "r.weigel@gmail.com", 15300, "Welcome123", true, false, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Brooklyn Nicholson", "214-555-2563", "b.nicholson@gmail.com", 15301,"Welcome123", true, false, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Stacy Anderews", "214-555-8153", "s.andrews@gmail.com", 15302, "Welcome123", true, false, "2018-10-01 6:23:00", "2018-10-01 6:23:00"),
+       ("Katelyn Nicholson", "214-555-8152", "kate.nicholson@gmail.com", 15303, "Welcome123", false, true, "2018-10-01 6:23:00", "2018-10-01 6:23:00")
+
+       
