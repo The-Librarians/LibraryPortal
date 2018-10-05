13b46ac4a3415975d7faf195a5943bd630e0bb9e
alex-bowen <alexbowen2017@gmail.com>
2018-10-05 18:04:01 -0500

    forms working

--- public/js/librariantabs.js
+++ public/js/librariantabs.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
-	
-	$('.librarian-button').click(function(){
+$(document).ready(function () {
+
+	$('.librarian-button').click(function () {
 		var form_id = $(this).attr('data-tab');
 
 		$('.librarian-form-button').removeClass('current');
 		$('.librarian-form-content').removeClass('current');
 
 		$(this).addClass('current');
-		$("#"+form_id).addClass('current');
+		$("#" + form_id).addClass('current');
 	});
 
+
+	// add a user
 	$("#inputUser").click(function () {
 		event.preventDefault();
 		var patronClassChoice = $("#patronClassification").val();
@@ -26,16 +28,45 @@ $(document).ready(function(){
 
 		var patronInput = {
 			name: $("#patronName").val(),
-			phone: $("#patronNumber").val(),
-			email: $("#cardNumber").val(),
-			cardNumber: $("#patronEmail").val(),
+			phone: $("#patronPhone").val(),
+			email: $("#patronEmail").val(),
+			cardNumber: $("#cardNumber").val(),
+			// temporary solution until we can implement authentication
+			password: "password",
 			isChild: isChild,
 			isAdult: isAdult
-		   }
+		}
 		
 		console.log(patronInput);
+		$.ajax("/api/users/", {
+			type: "POST",
+			data: patronInput
+		}).then(function () {
+			console.log("patron added")
+		})
 	});
 
+	// add a book
+	$("#inputBook").on("click", function (event) {
+		event.preventDefault();
+		console.log("click");
+		var bookInput = {
+			title: $("#bookTitleInput").val(),
+			author: $("#authorInput").val(),
+			genre: $("#genreInput").val(),
+			audience: $("#audienceInput").val(),
+			description: $("#descriptionInput").val(),
+			isCheckedOut: $("#checkedOut").val()
+		}
+		$.ajax("/api/books/", {
+			type: "POST",
+			data: bookInput
+		}).then(function () {
+			console.log("book added")
+		})
+	});
+
+	// add a dvd 
 	$("#inputMedia").click(function () {
 		event.preventDefault();
 
@@ -47,7 +78,13 @@ $(document).ready(function(){
 			audience: $("#mediaAudienceInput").val(),
 		}
 
-		console.log(mediaInput);
+		$.ajax("/api/media/", {
+			type: "POST",
+			data: mediaInput
+		}).then(function () {
+			console.log("media added")
+		})
+
 	})
 
 });
--- public/js/login.js
+++ public/js/login.js
@@ -1,48 +1,25 @@
-$(function() {
-  $("#landingExplorer").on("click", function(event) {
+$(function () {
+  $("#landingExplorer").on("click", function (event) {
     event.preventDefault();
     var userType = $("#exampleRecipientInput").val();
     var userInput = $("#patronLibrarianId").val();
     console.log(userType + userInput);
 
     //redirects to Patron Page
-    if (userType === "patron"){
-    window.location.href = "/users/" + userInput;
+    if (userType === "patron") {
+      window.location.href = "/users/" + userInput;
     }
-    else { 
+    else {
       window.location.href = "/books/";
     }
   });
- 
-  $("#catalogExplorer").on("click", function(event){
+
+  $("#catalogExplorer").on("click", function (event) {
     event.preventDefault();
-   var catalogExplorer = $("#catalog-search-bar").val();
+    var catalogExplorer = $("#catalog-search-bar").val();
     console.log(catalogExplorer)
 
-    window.location.href="/books/title/" + catalogExplorer
+    window.location.href = "/books/title/" + catalogExplorer
   })
 
-  
-    //Add new book1.
-    
-  $("#bookSubmit").on("click", function(event){
-    event.preventDefault();
-    console.log("click");
-    var bookInput = {
-     title : $("#bookTitleInput").val(),
-     author : $("#authorInput").val(),
-     genre : $("#genreInput").val(),
-     audience : $("#audienceInput").val(),
-     description : $("#descriptionInput").val(),
-     isCheckedOut : $("#checkedOut").val(),
-    
-    }
-    $.ajax("/api/books/",{
-      type:"POST",
-      data:bookInput
-    }).then(function(){
-      console.log("book added")
-    })
-   
-})
 })
