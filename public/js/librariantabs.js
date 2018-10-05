a79db5565a5aea3bdcd970eb8257e9e04e68f81d
alex-bowen <alexbowen2017@gmail.com>
2018-10-05 16:28:26 -0500

    Halfway thru forms

--- public/js/librariantabs.js
+++ public/js/librariantabs.js
@@ -8,6 +8,46 @@ $(document).ready(function(){
 
 		$(this).addClass('current');
 		$("#"+form_id).addClass('current');
+	});
+
+	$("#inputUser").click(function () {
+		event.preventDefault();
+		var patronClassChoice = $("#patronClassification").val();
+		var isChild;
+		var isAdult;
+
+		if (patronClassChoice == "Option 1") {
+			isChild = true;
+			isAdult = false;
+		} else {
+			isChild = false;
+			isAdult = true;
+		}
+
+		var patronInput = {
+			name: $("#patronName").val(),
+			phone: $("#patronNumber").val(),
+			email: $("#cardNumber").val(),
+			cardNumber: $("#patronEmail").val(),
+			isChild: isChild,
+			isAdult: isAdult
+		   }
+		
+		console.log(patronInput);
+	});
+
+	$("#inputMedia").click(function () {
+		event.preventDefault();
+
+		var mediaInput = {
+			title: $("#mediaTitleInput").val(),
+			author: $("#mediaAuthorInput").val(),
+			description: $("#mediaDescriptionInput").val(),
+			genre: $("#mediaGenreInput").val(),
+			audience: $("#mediaAudienceInput").val(),
+		}
+
+		console.log(mediaInput);
 	})
 
 });
